8905b8dadd617894f3eb940a881801b270b19498
alexandrahalchin <halchin.alexandra@gmail.com>
2014-01-23 11:06:23 +0100

    #25 - Create_Domain_Post

--- Musement/test/unit/musement/NotificationSpec.groovy
+++ Musement/test/unit/musement/NotificationSpec.groovy
@@ -9,12 +9,39 @@ import spock.lang.Specification
 @TestFor(Notification)
 class NotificationSpec extends Specification {
 
+    def post_received
+    def category1
+
     def setup() {
+        post_received = new Post()
+        category1 = new Category()
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "notification category cannot be empty"() {
+
+        def notificationWithCategory = new Notification( concernedPost: post_received, category: category1)
+        def notificationWithoutCategory = new Notification( concernedPost: post_received )
+
+        expect:
+        notificationWithCategory.validate()
+        notificationWithCategory.category == category1
+        !notificationWithoutCategory.validate()
+
+    }
+
+    void "notification post cannot be empty " () {
+        def notificationWithPost = new Notification(concernedPost: post_received, category: category1 )
+        def notificationWithoutPost = new Notification()
+
+        expect:
+        notificationWithPost.validate()
+        notificationWithPost.concernedPost == post_received
+        notificationWithPost.category == category1
+        !notificationWithoutPost.validate()
+
+
     }
 }
