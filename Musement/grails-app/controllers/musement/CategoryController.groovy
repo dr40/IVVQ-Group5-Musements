fa6f5648563b6f6afeb86961f738dc9ab89ae11d
Oudom <kemoudom@gmail.com>
2014-02-02 12:59:13 +0100

    IVVQ-Group5-Musements-74 Manage Category

---
+++ Musement/grails-app/controllers/musement/CategoryController.groovy
@@ -0,0 +1,138 @@
+package musement
+
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.plugin.springsecurity.annotation.Secured
+import musement.user.*
+
+
+@Transactional(readOnly = true)
+class CategoryController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    SpringSecurityService springSecurityService
+    CategoryService categoryService
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    def index(Integer max) {
+
+        if(!verifyUserPrivilege()){
+            //?????????????
+            redirect uri: '/login/auth'
+        }else{
+            params.max = Math.min(max ?: 10, 100)
+            params.max = Math.min(max ?: 10, 100)
+            respond Category.list(params), model: [categoryInstanceCount: Category.count()]
+        }
+
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    def show(Category categoryInstance) {
+        respond categoryInstance
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    def create() {
+        respond new Category(params)
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    @Transactional
+    def save(Category categoryInstance) {
+        if (categoryInstance == null) {
+            notFound()
+            return
+        }
+
+        if (categoryInstance.hasErrors()) {
+            respond categoryInstance.errors, view: 'create'
+            return
+        }
+
+        //categoryInstance.save flush: true
+        categoryService.addCategory(categoryInstance)
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.created.message', args: [message(code: categoryInstance.name, default: 'Category'), categoryInstance.id])
+                redirect categoryInstance
+            }
+            '*' { respond categoryInstance, [status: CREATED] }
+        }
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    def edit(Category categoryInstance) {
+        respond categoryInstance
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    @Transactional
+    def update(Category categoryInstance) {
+        if (categoryInstance == null) {
+            notFound()
+            return
+        }
+
+        if (categoryInstance.hasErrors()) {
+            respond categoryInstance.errors, view: 'edit'
+            return
+        }
+
+        categoryInstance.save flush: true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.updated.message', args: [message(code: Category.name, default: 'Category'), categoryInstance.id])
+                redirect categoryInstance
+            }
+            '*' { respond categoryInstance, [status: OK] }
+        }
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    @Transactional
+    def delete(Category categoryInstance) {
+
+        if (categoryInstance == null) {
+            notFound()
+            return
+        }
+
+        categoryInstance.delete flush: true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: Category.name, default: 'Category'), categoryInstance.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: categoryInstance.name, default: 'Category'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NOT_FOUND }
+        }
+    }
+
+    protected boolean verifyUserPrivilege(){
+        User user = (User) springSecurityService.currentUser
+        if ( user.getUsername() == 'admin'){
+            flash.message = 'admin'
+            return true
+        }
+        false
+
+    }
+}
